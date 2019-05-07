45e5af3f35e690aa4c2a52ac030de491b94bc77e
kashsid <kashifsiddique@gmail.com>
2019-05-07 15:41:32 -0500

    creates a function to print hellow word--> creates a function to add 2 numbers

--- helloworld.java
+++ helloworld.java
@@ -1,1 +1,21 @@
 //java hello world example
+
+//import java.util.Scanner;
+
+class HelloWorld
+{
+
+    public static void main(String args[]){
+        System.out.println("Hello World");
+        System.out.println("oh yeah my first java program");
+        System.out.println(result(2,3));
+    }
+
+    public static int result(int num1,int num2){
+        int resultNum= (num1+ num2);
+        return resultNum;
+    }
+
+
+
+}
